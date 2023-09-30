e9527e9741b52b740e375da1741044260c77dfd5
JeongJinbong(NoteBook) <jbong0104@naver.com>
2023-10-01 00:20:49 +0900

    Update Drill

--- Drill6.py
+++ Drill6.py
@@ -9,27 +9,25 @@ character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
 running = True
-Click = False
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 cx, cy = random.randint(0,1200), random.randint(0,1100)
 hx, hy = cx, cy
-tempx, tempy = 0, 0
+tmpX, tmpY = 0, 0
 frame = 0
 
 def handle_events():
     global running
     global hx, hy
-    global tempx, tempy
+    global tmpX, tmpY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            tempx, tempy = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN == SDL_BUTTON_LEFT:
-            hx, hy = tempx, tempy
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                hx, hy = event.x, TUK_HEIGHT - 1 - event.y
 
 def movetohand():
     global cx, cy
@@ -41,7 +39,6 @@ def movetohand():
         clear_canvas()
         TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
         hand_arrow.draw(hx, hy)
-        Click = False
         t = i/100
         movex = (1-t) * x1 + t * x2
         movey = (1-t) * y1 + t * y2
