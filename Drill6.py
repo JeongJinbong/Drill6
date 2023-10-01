69f60b993311d9be0c965dddf91c3b0b291188fa
JeongJinbong(NoteBook) <jbong0104@naver.com>
2023-10-01 19:48:28 +0900

    Update Click

--- Drill6.py
+++ Drill6.py
@@ -12,13 +12,13 @@ running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 cx, cy = random.randint(0,1200), random.randint(0,1100)
 hx, hy = cx, cy
-tmpX, tmpY = 0, 0
 frame = 0
-
+moveto = [[cx, cy]]
+Start = False
 def handle_events():
     global running
     global hx, hy
-    global tmpX, tmpY
+    global Start
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -28,31 +28,34 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == SDL_BUTTON_LEFT:
                 hx, hy = event.x, TUK_HEIGHT - 1 - event.y
+                moveto.append([hx, hy])
 
 def movetohand():
     global cx, cy
     global hx, hy
     global frame
     x1, y1 = cx, cy
-    x2, y2 = hx, hy
+    x2, y2 = moveto[0][0], moveto[0][1]
     for i in range(0, 100, 1):
+        handle_events()
         clear_canvas()
         TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-        hand_arrow.draw(hx, hy)
+        for j in range(0, len(moveto)):
+            hand_arrow.draw(moveto[j][0], moveto[j][1])
         t = i/100
         movex = (1-t) * x1 + t * x2
         movey = (1-t) * y1 + t * y2
-        if cx < hx:
+        if cx < moveto[0][0]:
             character.clip_draw(frame * 100, 100 * 1, 100, 100, movex, movey)
-        elif cx > hx:
+        elif cx > moveto[0][0]:
             character.clip_draw(frame * 100, 0 * 1, 100, 100, movex, movey)
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.016)
         if i == 100-1:
-            cx, cy = hx, hy
+            cx, cy = moveto[0][0], moveto[0][1]
+            moveto.pop(0)
 
 while running:
     movetohand()
-    handle_events()
 close_canvas()
